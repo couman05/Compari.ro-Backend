2d974240bce38d45cb19c8af31a777e614b6ee19
couman05 <emanuelcoman05@yahoo.com>
2023-11-20 14:23:03 +0200

    Implemented other fields for the entity class

--- src/main/java/com/youtube/jwt/entityMongo/Product.java
+++ src/main/java/com/youtube/jwt/entityMongo/Product.java
@@ -4,15 +4,22 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document(collection = "laptops")
 @Data
 public class Product {
     @Id
     private String id;
     private String site;
+    private String category;
     private String title;
     private double price;
     private String link;
+    private List<String> image_urls;
+    private List<String> reviews;
+    private List<String> description;
+    private List<String> users;
 
     public void updateProduct(Product newProduct) {
         if (newProduct.getSite() != null) {
@@ -27,6 +34,22 @@ public class Product {
         if (newProduct.getLink() != null) {
             this.link = newProduct.getLink();
         }
+        if(newProduct.getCategory() !=null){
+            this.category=newProduct.getCategory();
+        }
+        if(newProduct.getImage_urls() !=null){
+            this.image_urls=newProduct.getImage_urls();
+        }
+        if(newProduct.getReviews() !=null){
+            this.reviews=newProduct.getReviews();
+        }
+        if(newProduct.getDescription() !=null){
+            this.description=newProduct.getDescription();
+        }
+        if(newProduct.getUsers() !=null){
+            this.users=newProduct.getUsers();
+        }
+
     }
 
 }
